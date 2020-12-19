6323afd84c4a598b3a527df0827a22154c6f12d1
thallesyasmim <thallesgabriel1307@gmail.com>
2020-12-19 14:19:41 -0300

    User Relationship

--- app/Models/Tweet.js
+++ app/Models/Tweet.js
@@ -4,6 +4,9 @@
 const Model = use('Model')
 
 class Tweet extends Model {
+    user() { // O nome do relacionamento pode ser qualquer um
+        return this.belongsTo('App/Models/User')
+    }
 }
 
 module.exports = Tweet
