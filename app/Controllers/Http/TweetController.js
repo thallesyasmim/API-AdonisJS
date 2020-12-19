08a26d47148b8a517571422926dad9dfa6ddccc5
thallesyasmim <thallesgabriel1307@gmail.com>
2020-12-19 14:23:38 -0300

    Use Relationship In Index Method

--- app/Controllers/Http/TweetController.js
+++ app/Controllers/Http/TweetController.js
@@ -20,7 +20,7 @@ class TweetController {
    * @param {View} ctx.view
    */
   async index () {
-    const tweets = await Tweet.all()
+    const tweets = await Tweet.query().with('user').fetch()
     return tweets
   }
 
